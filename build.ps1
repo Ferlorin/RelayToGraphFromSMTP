7a2f7e9c4a5a3ad0a871a59a68f650901278f196
Ferlorin <harrytsirikos@yahoo.gr>
2025-03-07 16:48:56 +0200

    maybe without debugging symbols

--- build.ps1
+++ build.ps1
@@ -1,15 +1,15 @@
 Write-Host "Building Linux:"
 $env:GOOS="linux"
 $env:GOARCH="amd64"
-go build -o smtpservice
+go build -ldflags="-s -w" -o smtpservice
 
 Write-Host "Building Windows x64:"
 $env:GOOS="windows"
 $env:GOARCH="amd64"
-go build -o smtpservice_x64.exe
+go build -ldflags="-s -w" -o smtpservice_x64.exe
 
 Write-Host "Building Windows x86:"
 $env:GOARCH="386"
-go build -o smtpservice_x86.exe
+go build -ldflags="-s -w" -o smtpservice_x86.exe
 
 Write-Host "Builds complete: smtpservice smtpservice_x64.exe and smtpservice_x86.exe"
